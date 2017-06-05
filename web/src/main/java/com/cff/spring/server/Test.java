cb43d373387ffc09674d1561822070deaec5a5df
chenff <chenff@uyunsoft.cn>
2017-06-06 00:41:45 +0800

    初始化项目

---
+++ web/src/main/java/com/cff/spring/server/Test.java
@@ -0,0 +1,11 @@
+package com.cff.spring.server;
+
+/**
+ * Created by cff on 2017/6/6.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("asdasasds'");
+    }
+}
---
+++ web/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!asdasd</h2>
+</body>
+</html>
