1d81f4692b2b3fe93b70834f4a4eaf10e604c93d
chenff <chenff@uyunsoft.cn>
2017-06-12 23:43:47 +0800

    删除和新增测试

--- web/src/main/test/Test2.java
+++ web/src/main/test/Test2.java
@@ -4,7 +4,7 @@ import com.cff.spring.spi.SpiTestApi;
 /**
  * Created by cff on 2017/6/8.
  */
-public class Test {
+public class Test2 {
 
     public static void main(String[] args) {
         SpiTestApi spi = SpiFactory.getSpiFactory();
@@ -13,6 +13,6 @@ public class Test {
         } else {
             System.out.println("null");
         }
-
+        System.out.println("测试修改");
     }
 }
