56af8ffb1b778a1bd110eb34b5fd5af27f7f7750
chenff <chenff@uyunsoft.cn>
2017-10-31 14:45:07 +0800

    测试代码提交

--- web/src/main/java/com/cff/spring/action/SpringMvcTestAction.java
+++ web/src/main/java/com/cff/spring/action/SpringMvcTestAction.java
@@ -1,10 +1,14 @@
 package com.cff.spring.action;
 
 import com.cff.spring.entity.User;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.NativeWebRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -22,8 +26,12 @@ public class SpringMvcTestAction {
         return new User();
     }
 
+    // 数据解析,可以将date数据进行处理
     @InitBinder
     public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
         System.out.println("============应用到所有@RequestMapping注解方法，在其执行之前初始化数据绑定器");
     }
 
--- web/src/main/java/com/cff/spring/action/TestAction.java
+++ web/src/main/java/com/cff/spring/action/TestAction.java
@@ -1,15 +1,15 @@
 package com.cff.spring.action;
 
+import com.cff.spring.mvc.impls.IAutomation;
+import com.cff.spring.service.test.MyCondition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.Clock;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.IsoFields;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 /**
@@ -19,68 +19,40 @@ import java.time.temporal.IsoFields;
  * @date 2017/6/19 下午11:44
  */
 @Controller
-@RequestMapping("/cff/test")
+@RequestMapping("/test")
 public class TestAction {
 
+    @Autowired
+    private IAutomation automation;
+
+    @RequestMapping("/init")
+    @ResponseBody
+    public String initTest(String pwd){
+    	MyCondition.myMap.put("myCondition", "cff");
+        return pwd;
+    }
 
     @RequestMapping("/test1")
     @ResponseBody
     public String test1(String pwd) {
-        return pwd;
+        System.out.println(automation);
+        if(automation==null){
+            return "null";
+        }
+        return automation.say(pwd);
+    }
+    
+    @RequestMapping("/error")
+    @ResponseBody
+    public void notfount(HttpServletRequest request,HttpServletResponse response) {
+    	try {
+            System.out.println("执行了error");
+            response.sendRedirect("/index.jsp");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
     }
 
-    public static void main(String[] args) {
-        //自定义时区
-        LocalDateTime now2 = LocalDateTime.now(ZoneId.of("Europe/Paris"));
-        System.out.println("now2--"+now2);//会以相应的时区显示日期
-
-        //自定义时钟
-        Clock clock = Clock.system(ZoneId.of("Asia/Dhaka"));
-        LocalDateTime now3 = LocalDateTime.now(clock);
-        System.out.println("now3--"+now3);//会以相应的时区显示日期
-
-        //不需要写什么相对时间 如java.util.Date 年是相对于1900 月是从0开始
-        //2013-12-31 23:59
-        LocalDateTime d1 = LocalDateTime.of(2013, 12, 31, 23, 59);
-        System.out.println("d1--"+d1);
-        //年月日 时分秒 纳秒
-        LocalDateTime d2 = LocalDateTime.of(2013, 12, 31, 23, 59, 59, 11);
-        System.out.println("d2--"+d2);
-        //使用瞬时时间 + 时区
-        Instant instant = Instant.now();
-        LocalDateTime d3 = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
-        System.out.println("d3--"+d3);
-
-        //解析String--->LocalDateTime
-        LocalDateTime d4 = LocalDateTime.parse("2013-12-31T23:59");
-        System.out.println("d4--"+d4);
-
-        LocalDateTime d5 = LocalDateTime.parse("2013-12-31T23:59:59.999");//999毫秒 等价于999000000纳秒
-        System.out.println("d5--"+d5);
-        System.out.println("--------------");
-        //使用DateTimeFormatter API 解析 和 格式化
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime d6 = LocalDateTime.parse("2013/12/31 23:59:59", formatter);
-        System.out.println(formatter.format(d6));
-
-
-        //时间获取
-        System.out.println(d6.getYear());
-        System.out.println(d6.getMonth());
-        System.out.println(d6.getDayOfYear());
-        System.out.println(d6.getDayOfMonth());
-        System.out.println(d6.getDayOfWeek());
-        System.out.println(d6.getHour());
-        System.out.println(d6.getMinute());
-        System.out.println(d6.getSecond());
-        System.out.println(d6.getNano());
-        //时间增减
-        LocalDateTime d7 = d6.minusDays(1);
-        LocalDateTime d8 = d7.plus(1, IsoFields.QUARTER_YEARS);
-
-        //LocalDate 即年月日 无时分秒
-        //LocalTime即时分秒 无年月日
-        //API和LocalDateTime类似就不演示了
-    }
 }
 
